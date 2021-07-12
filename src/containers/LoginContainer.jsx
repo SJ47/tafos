e7ba1b44f3360e3525a7e7e81b3b35a2b7e98fea
Scott Johnston <scott@umachan.co.uk>
2021-07-12 19:43:04 +0100

    Sample login page from materialUI added

---
+++ src/containers/LoginContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import SignIn from "../components/SignIn";
+const LoginContainer = () => {
+    return (
+        <>
+            <SignIn />
+        </>
+    );
+};
+
+export default LoginContainer;
